8a17f042ce05277336f7bf880d433c96de3b1d02
Igor Durica <xduricai@stuba.sk>
2024-02-11 15:59:37 +0100

    Navbar adjustments

--- qck-store-fe/src/navigation/Navbar.tsx
+++ qck-store-fe/src/navigation/Navbar.tsx
@@ -1,27 +1,35 @@
+import './navigation.css';
 import { Searchbar } from './Searchbar';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import SettingsIcon from '@mui/icons-material/Settings';
 import { Logo } from './Logo';
-import styles from './navigation.module.css';
 import { IconButton } from '../shared/IconButton';
+import { useUserContext } from '../UserContext';
  
 export function Navbar(): JSX.Element {
+    const userContext = useUserContext();
+    
     return (
-        <nav className={`${styles.navbar} shadow-lg shadow-zinc-600/30 items-center w-full align-items-center text-xl font-medium h-16 bg-violet-800`}>
+        <nav className='navbar shadow-lg shadow-zinc-600/30 items-center w-full align-items-center text-xl font-medium h-16 bg-violet-800'>
             {/* <Link to={'/'}>Home</Link> */}
             <Logo />
-            <div className='ml-4'>
-                <Searchbar />
-            </div>
-            <div className='flex flex-row mx-4 w-min'>
-                <IconButton>
-                    <SettingsIcon fontSize='large' className='text-white' />
-                </IconButton>
+            {!!userContext.user &&
+            <>
+                <div className='ml-4'>
+                    <Searchbar />
+                </div>
                 
-                <IconButton>
-                    <AccountCircleIcon fontSize='large' className='text-white' />
-                </IconButton>
-            </div>
+                <div className='flex flex-row mx-4 w-min'>
+                    <IconButton>
+                        <SettingsIcon fontSize='large' className='text-white' />
+                    </IconButton>
+                    
+                    <IconButton>
+                        <AccountCircleIcon fontSize='large' className='text-white' />
+                    </IconButton>
+                </div>
+            </>
+            }
         </nav>
     )
 }

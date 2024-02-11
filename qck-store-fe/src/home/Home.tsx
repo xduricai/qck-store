d547c44f12850d7628be9a2ffa669599f5de167e
Igor Durica <xduricai@stuba.sk>
2024-02-11 18:57:09 +0100

    Added empty components

---
+++ qck-store-fe/src/home/Home.tsx
@@ -0,0 +1,5 @@
+export function Home() {
+    return (
+        <h2>HOME</h2>
+    )
+}
---
+++ qck-store-fe/src/registration/Registration.tsx
@@ -0,0 +1,5 @@
+export function Registration() {
+    return (
+        <h2>Registration component</h2>
+    );
+}

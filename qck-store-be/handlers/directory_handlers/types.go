97afc373684c85b350e7b52be8f311c09441e9a8
Igor Durica <xduricai@stuba.sk>
2024-03-09 16:59:45 +0100

    File response struct

--- qck-store-be/handlers/directory_handlers/types.go
+++ qck-store-be/handlers/directory_handlers/types.go
@@ -4,3 +4,11 @@ type DirectoryResponse struct {
 	Id   int    `json:"id"`
 	Name string `json:"name"`
 }
+
+type FileResponse struct {
+	Id       int    `json:"id"`
+	Size     int    `json:"size"`
+	Name     string `json:"name"`
+	Modified string `json:"modified"`
+	Created  string `json:"created"`
+}

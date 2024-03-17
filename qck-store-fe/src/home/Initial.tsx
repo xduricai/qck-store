71e8745ada55009fdef3d5e666eb74df15639b9a
Igor Durica <xduricai@stuba.sk>
2024-03-17 12:16:12 +0100

    Adjustments to visual + loading pages

--- qck-store-fe/src/home/Initial.tsx
+++ qck-store-fe/src/home/Initial.tsx
@@ -1,12 +1,18 @@
+import { useNavigate } from "react-router-dom";
 import { Button } from "../shared/Button";
 
 export function Initial() {
+    const navigate = useNavigate();
+    const toLogin = () => navigate('/login');
+    const toRegister = () => navigate('/register');
+    
     return (
-        <div className="flex flex-col h-[calc(100vh-64px)] text-center justify-center items-center">
-            <p>QCK STORE</p>
-            <Button label="Create an Account" />
+        <div className="flex gap-2 flex-col h-[calc(100vh-64px)] text-center justify-center items-center">
+            {/*TODO replace with logo*/}
+            <h2 className="text-xl text-purple-800 font-bold">QCK STORE</h2>
+            <Button onClick={toRegister} className="w-[200px]" color="outlined" label="Create an Account" />
             <p>Already have an account?</p>
-            <Button label="Sign In" />
+            <Button onClick={toLogin} className="w-[200px]" color="accent" label="Sign In" />
         </div>
     );
 }
--- qck-store-fe/src/home/Loading.tsx
+++ qck-store-fe/src/home/Loading.tsx
@@ -1,9 +1,13 @@
 export function Loading() {
     return (
-        <div className="flex h-[calc(100vh-64px)] text-center justify-center items-center">
-            PLACEHOLDER 
-            <br />
-            LOADING
+        <div className="flex flex-col h-[calc(100vh-64px)] text-center justify-center items-center font-bold">
+            {/*TODO replace with something else*/}
+            <h2 className="text-purple-800 text-xl">
+                QCK STORE
+            </h2>
+            <span className="text-lg">
+                LOADING DATA
+            </span>
         </div>
     );
 }

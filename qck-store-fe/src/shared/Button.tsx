99d75de3b15fbf310ec2e4b090ee73077c2689e2
Igor Durica <xduricai@stuba.sk>
2024-04-25 19:07:22 +0200

    Added extra button style

--- qck-store-fe/src/shared/Button.tsx
+++ qck-store-fe/src/shared/Button.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from "react";
 type ButtonProps = {
     children?: ReactNode;
     onClick?: () => unknown;
-    color?: "plain" | "outlined" | "accent";
+    color?: "plain" | "outlined" | "accent" | "warning";
     className?: string
     form?: string
     width?: string;
@@ -14,6 +14,7 @@ export function Button({ children, onClick, className, form, color="plain", widt
         plain: "border border-gray-400 hover:border-gray-800 hover:bg-gray-100",
         outlined: "text-purple-800 border border-purple-800 hover:bg-gray-100",
         accent: "bg-purple-800 text-slate-50 hover:bg-purple-900",
+        warning: "bg-red-800 text-slate-50 hover:bg-red-900",
     };
     
     return <button 

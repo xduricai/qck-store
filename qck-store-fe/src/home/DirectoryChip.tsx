04b09691fa4246e492dacd02b3446d74525790aa
Igor Durica <xduricai@stuba.sk>
2024-04-25 19:01:02 +0200

    Fixed folder chip navigation bug

--- qck-store-fe/src/home/DirectoryChip.tsx
+++ qck-store-fe/src/home/DirectoryChip.tsx
@@ -1,6 +1,6 @@
 import FolderIcon from '@mui/icons-material/Folder';
 import { Directory } from '../api/responses/Directory';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { MouseEvent } from 'react';
 import { ContextMenu, ContextMenuStatus } from './ContextMenu';
 
@@ -12,20 +12,29 @@ type DirectoryChipProps = {
 }
 
 export function DirectoryChip({ data, menuStatus, setMenuStatus, dirs = [] }: DirectoryChipProps) {
+    const navigate = useNavigate();
+    const toFolder = (id: number) => navigate(`/folder/${id}`);
+    
     function handleRightClick(event: MouseEvent) {
         event.preventDefault();
-        setMenuStatus({ id: data.id, type: "folder", x: event.pageX, y: event.pageY });
+        console.log(event)
+        setMenuStatus({ item: data, type: "folder", x: event.pageX, y: event.pageY });
     }
     
     return (
-        <Link onContextMenu={(event) => handleRightClick(event)} className="w-full" to={`/folder/${data.id}`}>
-        <div className="flex w-full border-gray-400 hover:border-gray-800 hover:bg-gray-100 border items-center rounded-lg p-2 cursor-pointer">
+        <>
+        <div 
+            onClick={() => toFolder(data.id)}
+            onContextMenu={(event) => handleRightClick(event)}
+            className="flex w-full border-gray-400 hover:border-gray-800 hover:bg-gray-100 border items-center rounded-lg p-2 cursor-pointer"
+        >
             <FolderIcon className="text-gray-800 mr-2" />
             <span className="truncate">{data.name}</span>
         </div>
-        {menuStatus?.id === data.id && menuStatus.type === "folder" && 
-            <ContextMenu dirs={dirs} menuStatus={menuStatus} />
+        {menuStatus?.item.id === data.id && menuStatus.type === "folder" && 
+            <ContextMenu dirs={dirs} menuStatus={menuStatus} setRename={() => {}} />
+            // TODO fix
         }
-        </Link>
+        </>
     );
 }

8e79ed8f03b96e6a6f7e3a47d286d786f0a449ee
xduricai <duricaigor@gmail.com>
2024-05-18 18:21:07 +0200

    Fixed sorting not working

--- qck-store-fe/src/home/Sorting.ts
+++ qck-store-fe/src/home/Sorting.ts
@@ -7,6 +7,14 @@ export type SortOption = {
     compareFn: ItemCompareFn;
 };
 
+function getDate(input: string) {
+    const segments = input.split("/");
+    if (segments.length !== 3) return new Date("");
+
+    const dateStr = `${segments[1]}/${segments[0]}/${segments[2]}`;
+    return new Date(dateStr);
+}
+
 function namesAsc<T extends File | Directory>(a: T, b: T): number {
     if (a.name === b.name) return 0;
     if (a.name < b.name) return -1;
@@ -21,16 +29,20 @@ function namesDesc<T extends File | Directory>(a: T, b: T): number {
 
 function datesAsc<T extends File | Directory>(a: T, b: T): number {
     if (a.modified === b.modified) return 0;
-    const dateA = new Date(a.modified);
-    const dateB = new Date(b.modified);
+    
+    const dateA = getDate(a.modified);
+    const dateB = getDate(b.modified);
+    
     if (dateA < dateB) return -1;
     return 1;
 }
 
 function datesDesc<T extends File | Directory>(a: T, b: T): number {
     if (a.modified === b.modified) return 0;
-    const dateA = new Date(a.modified);
-    const dateB = new Date(b.modified);
+
+    const dateA = getDate(a.modified);
+    const dateB = getDate(b.modified);
+    
     if (dateA > dateB) return -1;
     return 1;
 }
@@ -39,7 +51,7 @@ export function getSortOptions(): SortOption[] {
     return [
         { name: "Name: A-Z", compareFn: namesAsc },
         { name: "Name: Z-A", compareFn: namesDesc },
-        { name: "Newest", compareFn: datesAsc },
-        { name: "Oldest", compareFn: datesDesc },
+        { name: "Newest", compareFn: datesDesc },
+        { name: "Oldest", compareFn: datesAsc },
     ];
 }

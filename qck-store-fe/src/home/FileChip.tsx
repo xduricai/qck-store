f2e10e6fffeb44d31b4364ec88a739701f644b50
xduricai <duricaigor@gmail.com>
2024-05-18 18:07:09 +0200

    Fixed file name clipping

--- qck-store-fe/src/home/FileChip.tsx
+++ qck-store-fe/src/home/FileChip.tsx
@@ -18,12 +18,12 @@ export function FileChip({ data, setMenuStatus }: FileChipProps) {
     
     return (
         <div onContextMenu={(event) => handleRightClick(event)} className="flex flex-col aspect-square w-full border-gray-400 hover:border-gray-800 hover:bg-gray-100 border items-center rounded-lg p-2">
-            <div className="flex flex-row w-full items-center self-end">
-                <div className="flex flex-col">
+            <div className="flex flex-row w-full items-center justify-between self-end">
+                <div className="flex flex-col max-w-[calc(100%-1.5rem)]">
                     <span className="truncate">{data.name}</span>
                     <span className="truncate text-gray-700 text-sm">Size: {data.size}B</span>
                 </div>
-                <MoreVertIcon onClick={(event) => handleRightClick(event)} className="ml-auto text-gray-800 cursor-pointer" />
+                <MoreVertIcon onClick={(event) => handleRightClick(event)} className="text-gray-800 cursor-pointer" />
             </div>
             <div className="flex items-center justify-center w-full h-[calc(100%-0.5rem)] mt-2 rounded-lg bg-gray-300">
                 <DescriptionOutlinedIcon sx={{ width: 4/5, height: 4/5, strokeWidth:"1.25%" }} className="text-gray-400" />

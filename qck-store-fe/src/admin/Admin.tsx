9268e7f2504b1ad6be2873e22805492bd2ddbf59
xduricai <duricaigor@gmail.com>
2024-06-09 10:56:33 +0200

    Admin pagination FE WIP

--- qck-store-fe/src/admin/Admin.tsx
+++ qck-store-fe/src/admin/Admin.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { getUsers } from "../api/AdminClient";
 import { LoadingPage } from "../shared/Loading";
@@ -6,31 +7,45 @@ import { UserCard } from "./UserCard";
 import { Searchbar } from "../navigation/Searchbar";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import { IconButton } from "../shared/IconButton";
-import { Close } from "@mui/icons-material";
+import { Close, FirstPage, LastPage, NavigateBefore, NavigateNext } from "@mui/icons-material";
 
 export function Admin() {
+    const { query } = useParams();
     const { data, isLoading, isError } = useQuery({
         queryKey: ["users"],
         queryFn: getUsers
     });
+    const [ page, setPage ] = useState<number>(1);
     const navigate = useNavigate();
-    const { query } = useParams();
-    const title = query ? `Results for "${query}"` : "Users";
     
     if (isLoading) return <LoadingPage />
-    if (isError) return <ErrorPage />
+    if (isError || !data) return <ErrorPage />
+
+    const title = query ? `Results for "${query}"` : "Users";
+    let content = [...data];
+
+    if (query) {
+        content = content.filter(user => (
+            user.username.toLocaleLowerCase().includes(query) ||
+            user.email.toLocaleLowerCase().includes(query) ||
+            user.firstName.toLocaleLowerCase().includes(query) ||
+            user.lastName.toLocaleLowerCase().includes(query)
+        ));
+    }
+    const pageCount = Math.ceil(content.length / 10);
+    if (page > pageCount) setPage(pageCount);
+    content = content.slice((page - 1) * 10, page * 10)
 
     function search(query: string) {
         if (!query) return;
-        navigate(`/${query}`);
+        navigate(`/search/${query.toLocaleLowerCase()}`);
     }
     
-    //TODO add search and pagination
     return (
         <div className="h-[calc(100%-4rem)] max-w-[900px] px-12 mx-auto">
             <div className="flex items-center">
                 <h1 className="text-xl font-semibold my-6 mr-1">{title}</h1>
-                {query && 
+            {query && 
                     <Link to="/">
                         <IconButton>
                             <Close className="text-purple-800"/>
@@ -40,8 +55,27 @@ export function Admin() {
                 <Searchbar className="ml-auto" onSubmit={search} />
             </div>
             <section className="flex flex-col gap-y-3">
-                {data?.map(user => <UserCard key={user.id} user={user} /> )}
+                {content.map(user => <UserCard key={user.id} user={user} /> )}
             </section>
+            <div className="flex items-center border-t border-gray-400 mt-4 p-2">
+                <p className="text-sm ml-auto mr-4 mt-0.5">
+                    Showing {(page - 1) * 10 + 1} - {Math.min(page * 10, data.length)} of {data.length}
+                </p>
+                <p className="flex">
+                    <IconButton disabled={page === 1} onClick={() => setPage(1)}>
+                        <FirstPage className={page === 1 ? "text-gray-400" : "text-gray-600"} />
+                    </IconButton>
+                    <IconButton disabled={page === 1} onClick={() => setPage(page - 1)}>
+                        <NavigateBefore className={page === 1 ? "text-gray-400" : "text-gray-600"} />
+                    </IconButton>
+                    <IconButton disabled={page === pageCount} onClick={() => setPage(page + 1)}>
+                        <NavigateNext className={page === pageCount ? "text-gray-400" : "text-gray-600"} />
+                    </IconButton>
+                    <IconButton disabled={page === pageCount} onClick={() => setPage(pageCount)}>
+                        <LastPage className={page === pageCount ? "text-gray-400" : "text-gray-600"} />
+                    </IconButton>
+                </p>
+            </div>
         </div>          
     );
 }
